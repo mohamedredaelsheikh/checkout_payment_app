f928f531b2efc8a959c786d4997909d690550622
mohamedredaelsheikh <moh.reda198@gmail.com>
2024-07-26 23:57:27 +0300

    create payment method.

--- lib/features/checkout_payment/presentation/views/widgets/payment_details_view_body.dart
+++ lib/features/checkout_payment/presentation/views/widgets/payment_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:checkout_payment_app/features/checkout_payment/presentation/views/widgets/payment_method_item.dart';
 import 'package:flutter/material.dart';
 
 class PaymentDetailsViewBody extends StatelessWidget {
@@ -5,8 +6,11 @@ class PaymentDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [],
-    );
+    return const Column(children: [
+      PaymentMethodItem(
+        image: "assets/images/card.svg",
+        isActive: true,
+      )
+    ]);
   }
 }
---
+++ lib/features/checkout_payment/presentation/views/widgets/payment_method_item.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class PaymentMethodItem extends StatelessWidget {
+  const PaymentMethodItem(
+      {super.key, this.isActive = false, required this.image});
+  final bool isActive;
+  final String image;
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 600),
+      width: 103,
+      height: 62,
+      decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+            side: BorderSide(
+              color:
+                  isActive ? const Color(0xff34A853) : const Color(0xff000000),
+              width: 1.5,
+            ),
+          ),
+          shadows: [
+            BoxShadow(
+              color: isActive ? const Color(0xff34A853) : Colors.white,
+              blurRadius: 4,
+              offset: const Offset(0, 0),
+              spreadRadius: 0,
+            )
+          ]),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Colors.white,
+        ),
+        child: Center(
+          child: SvgPicture.asset(
+            image,
+            height: 24,
+          ),
+        ),
+      ),
+    );
+  }
+}
