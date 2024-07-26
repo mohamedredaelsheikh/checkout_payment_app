c98c77366251334a0c5740715c4ecc8ed748ddd9
mohamedredaelsheikh <moh.reda198@gmail.com>
2024-07-26 16:15:26 +0300

    Create app Structure.

---
+++ android/app/src/main/kotlin/com/example/checkout_payment_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.checkout_payment_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:checkout_payment_app/features/checkout_payment/presentation/views/my_card_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CheckOutPaymentApp());
+}
+
+class CheckOutPaymentApp extends StatelessWidget {
+  const CheckOutPaymentApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyCardView(),
+    );
+  }
+}
