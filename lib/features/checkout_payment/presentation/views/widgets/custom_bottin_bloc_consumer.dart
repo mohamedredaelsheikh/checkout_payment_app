6ac6affd54683498b553bd59c8175e430a6257f5
mohamedredaelsheikh <moh.reda198@gmail.com>
2024-11-05 13:56:13 +0200

    create amount model and items list model

--- lib/features/checkout_payment/presentation/views/widgets/custom_bottin_bloc_consumer.dart
+++ lib/features/checkout_payment/presentation/views/widgets/custom_bottin_bloc_consumer.dart
@@ -1,9 +1,12 @@
+import 'dart:developer';
+
 import 'package:checkout_payment_app/features/checkout_payment/data/models/payment_intent_model_input.dart';
 import 'package:checkout_payment_app/features/checkout_payment/presentation/manger/cubit/stripe_payment_cubit.dart';
 import 'package:checkout_payment_app/features/checkout_payment/presentation/views/thank_you_view.dart';
 import 'package:checkout_payment_app/features/checkout_payment/presentation/views/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 
 class CustomButtonBlocConsumer extends StatelessWidget {
   const CustomButtonBlocConsumer({
@@ -29,13 +32,80 @@ class CustomButtonBlocConsumer extends StatelessWidget {
       builder: (context, state) {
         return CustomButton(
             onTap: () {
-              PaymentIntentInputModel paymentIntentInputModel =
+              /* PaymentIntentInputModel paymentIntentInputModel =
                   PaymentIntentInputModel(
                       amount: "100",
                       currency: "USD",
                       customerId: "cus_RA2dmoz6BvrGgS");
               BlocProvider.of<StripePaymentCubit>(context).makePayment(
                   paymentIntentInputModel: paymentIntentInputModel);
+            */
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (BuildContext context) => PaypalCheckoutView(
+                  sandboxMode: true,
+                  clientId: "YOUR CLIENT ID",
+                  secretKey: "YOUR SECRET KEY",
+                  transactions: const [
+                    {
+                      "amount": {
+                        "total": "100",
+                        "currency": "USD",
+                        "details": {
+                          "subtotal": "100",
+                          "shipping": "0",
+                          "shipping_discount": 0
+                        }
+                      },
+                      "description": "The payment transaction description.",
+                      // "payment_options": {
+                      //   "allowed_payment_method":
+                      //       "INSTANT_FUNDING_SOURCE"
+                      // },
+                      "item_list": {
+                        "items": [
+                          {
+                            "name": "Apple",
+                            "quantity": 4,
+                            "price": '10',
+                            "currency": "USD"
+                          },
+                          {
+                            "name": "Pineapple",
+                            "quantity": 5,
+                            "price": '12',
+                            "currency": "USD"
+                          }
+                        ],
+
+                        // Optional
+                        //   "shipping_address": {
+                        //     "recipient_name": "Tharwat samy",
+                        //     "line1": "tharwat",
+                        //     "line2": "",
+                        //     "city": "tharwat",
+                        //     "country_code": "EG",
+                        //     "postal_code": "25025",
+                        //     "phone": "+00000000",
+                        //     "state": "ALex"
+                        //  },
+                      }
+                    }
+                  ],
+                  note: "Contact us for any questions on your order.",
+                  onSuccess: (Map params) async {
+                    log("onSuccess: $params");
+                    Navigator.pop(context);
+                  },
+                  onError: (error) {
+                    log("onError: $error");
+                    Navigator.pop(context);
+                  },
+                  onCancel: () {
+                    print('cancelled:');
+                    Navigator.pop(context);
+                  },
+                ),
+              ));
             },
             isLoading: State is StripePaymentLoading ? true : false,
             text: "Continue");
